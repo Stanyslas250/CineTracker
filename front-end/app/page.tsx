eedcb6236238881f7b359002ad8baa28b9e0de4b
Stanyslas250 <stanyslas.contact@gmail.com>
2024-10-19 16:58:48 +0200

    Adding a loading to wait user information

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+import { Spinner } from "@nextui-org/spinner";
+
 import { Navbar } from "@/components/navbar";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Home() {
+  const { loading } = useAuth();
+
+  if (loading)
+    return (
+      <section className="flex flex-col items-center justify-center h-screen w-screen">
+        <Spinner size="lg" />
+      </section>
+    );
+
   return (
     <section className="flex flex-col items-center justify-center">
       <Navbar />
