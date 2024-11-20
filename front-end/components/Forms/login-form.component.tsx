10af29bd915ad69c1746300744f778006ef2dc03
Stanyslas250 <stanyslas.contact@gmail.com>
2024-11-20 09:38:42 +0100

    Include react-hook-form to the login form

--- front-end/components/Forms/login-form.component.tsx
+++ front-end/components/Forms/login-form.component.tsx
@@ -1,21 +1,50 @@
 import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
+import { loginSchema } from '@/schemas/AuthSchemas';
+import { LoginData } from '@/types/AuthTypes';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
 
 export function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginData>({
+    resolver: zodResolver(loginSchema),
+  });
+  const onSubmit = async (data: LoginData) => {
+    console.log('SUCCESS', data);
+  };
   return (
     <>
-      <form className="flex flex-col gap-4 ">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
         <div>
           <div className="mb-2 block">
             <Label htmlFor="email1" value="Your email" />
           </div>
-          <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
+          <div>
+            <TextInput
+              id="email1"
+              type="email"
+              placeholder="name@example.com"
+              color={errors.email ? 'failure' : 'gray'}
+              helperText={errors.email ? <span>{errors.email.message}</span> : null}
+              {...register('email')}
+            />
+          </div>
         </div>
         <div>
           <div className="mb-2 block">
             <Label htmlFor="password1" value="Your password" />
           </div>
-          <TextInput id="password1" type="password" required />
+          <TextInput
+            id="password1"
+            type="password"
+            {...register('password')}
+            color={errors.password ? 'failure' : 'gray'}
+            helperText={errors.password ? <span>{errors.password.message}</span> : null}
+          />
         </div>
         <div className="flex items-center gap-2">
           <Checkbox id="remember" />
